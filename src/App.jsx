b7f54e5dfe1d3c68b51e6f072b801e672c0a6f15
GuillePfarr <guillepfarr@hotmail.com>
2023-09-24 15:56:22 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-layout">
+      <header className="app-header">Header</header>
+      <main className="app-main">Main</main>
+      <footer className="app-footer">Footer</footer>
+    </div>
+  )
+}
+
+export default App
