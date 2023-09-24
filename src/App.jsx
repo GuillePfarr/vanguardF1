960bc2820383b31cc133f827ae1f57e93ff95f77
GuillePfarr <guillepfarr@hotmail.com>
2023-09-24 16:24:24 -0300

    Start new components

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="app-layout">
-      <header className="app-header">Header</header>
-      <main className="app-main">Main</main>
-      <footer className="app-footer">Footer</footer>
-    </div>
+    <>
+    <Home/>
+    </>
   )
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <div className="app-layout">
+      <header className="app-header">Header</header>
+      <main className="app-main">Main</main>
+      <footer className="app-footer">Footer</footer>
+    </div>
+  )
+}
+
+export default Home
